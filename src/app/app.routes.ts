1d4212b0cea7650a0def887eb9b01d6b7690ed7f
rayen <meray496@gmail.com>
2025-12-19 21:14:46 +0100

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,25 @@
+import { Routes } from '@angular/router';
+import { AjouterproduitsComponent } from './ajouterproduits/ajouterproduits.component';
+import { GestionproduitsComponent } from './gestionproduits/gestionproduits.component';
+import { HomeComponent } from './home/home.component';
+
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'gestion-produits',
+    pathMatch: 'full'
+  },
+  {
+    path: 'gestion-produits',
+    component: GestionproduitsComponent
+  },
+  {
+    path: 'ajout-produit',
+    component: AjouterproduitsComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  }
+];
